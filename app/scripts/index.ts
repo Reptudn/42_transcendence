6a3c782bd2b69dde87edfb7628040020a6ee8a01
Reptudn <jonas.kauker2003@gmail.com>
2025-02-12 18:52:28 +0100

    switch pages without reloading the actual page

--- app/scripts/index.ts
+++ app/scripts/index.ts
@@ -1,17 +1,33 @@
 const fastify = require("fastify")()
 const fastifyView = require("@fastify/view")
+const path = require("path")
+const fastifyStatic = require("@fastify/static")
 
 fastify.register(fastifyView, {
   engine: {
     ejs: require("ejs")
+  },
+  options: {
+    context: {
+      get: (obj: any, prop: any) => obj && obj[prop]
+    }
   }
 })
 
-// asynchronous handler:
+fastify.get("/partial/:page", async (req: any, reply: any) => {
+  const page = req.params.page;
+  const dataSample = { name: "Jonas" }; // fetch data here
+  return reply.view(`pages/${page}.ejs`, dataSample);
+});
+
 fastify.get("/", async (req: any, reply: any) => {
   return reply.viewAsync("pages/index.ejs", { name: "Jonas" }, {
     layout: "layouts/basic.ejs"
   });
+})
+
+fastify.post("/login", async (req: any, reply: any) => {
+  return "hello"
 })
 
 fastify.listen({ port: 3000 }, (err: any) => {
