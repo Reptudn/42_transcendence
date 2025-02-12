debca1b8371445dc634f25553a9eab2e9d0b4720
Jonas Kauker <jkauker@fedora.fritz.box>
2025-02-12 19:33:41 +0100

    basic database

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,34 @@
 const fastify = require("fastify")()
+import { openDb } from "./database";
 
-fastify.get("/login", async (req: any, reply: any) => {
-    return "hello"
-})
+async function setupDb() {
+    const db = await openDb();
 
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT
+        )
+    `);
 
-fastify.listen({ port: 4242 }, (err: any) => {
-    if (err) throw err;
-    console.log(`Backend on port ${fastify.server.address().port}`);
-})
+    await db.run(`INSERT INTO users (name, email) VALUES (?, ?)`, ['John Doe', 'john@example.com']);
+
+    const users = await db.all(`SELECT * FROM users`);
+    console.log(users);
+}
+
+async function startServer() {
+    await setupDb();
+
+    fastify.get("/login", async (req: any, reply: any) => {
+        return "hello"
+    });
+
+    fastify.listen({ port: 4242 }, (err: any) => {
+        if (err) throw err;
+        console.log(`Backend on port ${fastify.server.address().port}`);
+    });
+}
+
+startServer();
