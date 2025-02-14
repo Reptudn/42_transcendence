ffe485559ce33bac0ab5123b019f3c25d279a878
jkauker <jkauker@student.42heilbronn.de>
2025-02-14 10:57:03 +0000

    simple basic easy to win pong and other stuff

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,4 +3,27 @@ async function loadPartialView(page) {
 	const html = await response.text();
 	console.log("switching to page: " + page);
 	document.getElementById('content').innerHTML = html;
+
+	if (page === 'game') {
+        const script = document.createElement('script');
+        script.src = '/static/js/pong.js';
+        script.defer = true;
+        document.body.appendChild(script);
+    }
+
 }
+
+function toggleDarkMode() {
+	document.body.classList.toggle('dark');
+	const isDarkMode = document.body.classList.contains('dark');
+	document.getElementById('darkModeToggle').textContent = isDarkMode ? 'Light Mode' : 'Dark Mode';
+}
+
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+setCookie('username', "urmom", 1)
