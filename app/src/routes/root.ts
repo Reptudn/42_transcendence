b57a8309bda1ebced5e1156d5a97a5ff487c340a
Reptudn <jonas.kauker2003@gmail.com>
2025-06-26 19:14:22 +0200

    updated 404 page a bit with the new design

--- app/src/routes/root.ts
+++ app/src/routes/root.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginAsync } from 'fastify';
 import { checkAuth } from '../services/auth/auth';
+import { error } from 'console';
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
 	fastify.get(
@@ -15,6 +16,20 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
 			);
 		}
 	);
+
+	fastify.setNotFoundHandler(async (req, reply) => {
+		const isAuthenticated = (await checkAuth(req, false, fastify)) != null;
+		return reply.code(404).view(
+			'error.ejs',
+			{
+				err_code: 404,
+				err_message: 'Page not found',
+				isAuthenticated,
+				t: req.t,
+			},
+			{ layout: 'layouts/basic.ejs' }
+		);
+	});
 };
 
 export default root;
